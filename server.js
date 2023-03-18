502cb101d925fba4cbf99261e7b66005bfad60d2
kandalog <asuasu.tee@gmail.com>
2023-03-18 20:23:12 +0900

    cors credentials

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "root" });
