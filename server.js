665c31e90d0c764add3437986f1dc2bc8ac022be
kandalog <asuasu.tee@gmail.com>
2023-03-18 21:18:16 +0900

    httpOnly true

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/set", (req, res) => {
   res.cookie("name", "tee", {
     sameSite: "none",
     secure: true,
-    httpOnly: false,
+    httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
   });
   res.json({ message: "root" });
