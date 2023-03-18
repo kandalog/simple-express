0e4c70700ebc3401ab55ddaddd4377c74ec1d485
kandalog <asuasu.tee@gmail.com>
2023-03-19 01:52:04 +0900

    secure

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       sameSite: "none",
-      secure: false,
+      secure: true,
       httpOnly: true,
       maxAge: 60 * 60 * 1000,
     },
