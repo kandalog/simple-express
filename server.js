2e29d3423861b24e64be47ae117a6af02c07abef
kandalog <asuasu.tee@gmail.com>
2023-03-19 04:43:06 +0900

    cookie-parser

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
-import session from "express-session";
+// import session from "express-session";
+import cookieSession from "cookie-session";
 
 const app = express();
 app.enable("trust proxy");
@@ -20,11 +21,9 @@ app.use(
 app.use(cookieParser());
 
 app.use(
-  session({
+  cookieSession({
     name: "session",
     secret: "secret",
-    resave: true,
-    saveUninitialized: true,
     cookie: {
       sameSite: "none",
       secure: true,
